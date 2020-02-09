7c21615f67c1b4950caf446f64357e9adcc84f1d
fred913 <41846274+fredisapig@users.noreply.github.com>
2020-02-09 18:58:11 +0800

    Add WebUI module

--- webui/__init__.py
+++ webui/__init__.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 from flask import *
+from ..main import get_bilibili_rank
 
 app = Flask("bilibili_rank_spider_ui")
 
@@ -13,3 +14,7 @@ def index_page():
     return render_template(
         "index.html"
     )
+
+
+def main():
+    app.run("0.0.0.0", )
