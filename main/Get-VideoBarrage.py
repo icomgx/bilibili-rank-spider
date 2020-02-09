cfe9b5efe7521c35af64ed00556d9911b24872d3
ZhangTai <code_keep@outlook.com>
2020-02-09 15:55:16 +0800

    错误修正

---
+++ main/Get-VideoBarrage.py
@@ -0,0 +1,34 @@
+import json
+import re
+import requests
+
+
+# 获取页面
+def download_page(url):
+    # 添加请求头
+    # User-Agent：浏览器
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/79.0.3945.88 Safari/537.36 '
+    }
+    res = requests.get(url=url, headers=headers)
+    return res
+
+    # 根据视频的av号获取cid
+    def get_cid(av):
+        av = av.strip('av')
+        uri = f'https://api.bilibili.com/x/player/pagelist?aid={av}&jsonp=jsonp'
+        res = download_page(uri)
+        res_text = res.text
+        res_dict = json.loads(res_text)
+        cid = res_dict['data'][0]['cid']
+        return cid
+
+    # 根据cid请求弹幕
+    def get_barrage(self, cid):
+        url = f'https://api.bilibili.com/x/v1/dm/list.so?oid={cid}'
+        res = download_page(url)
+        res_xml = res.content.decode('utf-8')
+        pattern = re.compile('<d.*?>(.*?)</d>')
+        dan_mu_list = pattern.findall(res_xml)
+        return dan_mu_list
--- main/Get-bilibili-rank.py
+++ main/Get-bilibili-rank.py
@@ -25,20 +25,22 @@ def job():
         visit = item.find('span', {'class': 'data-box'}).text
         up = item.find_all('a')[2].text
         up_id = item.find_all('a')[2].get('href')[len('//space.bilibili.com/'):]
-        url = item.find('a', {'class': 'title'}).get('href')
-        av_id = url[len('https://www.bilibili.com/video/'):]
+        uri = item.find('a', {'class': 'title'}).get('href')
+        av_id = uri[len('https://www.bilibili.com/video/'):]
         # 添加到数据库
-        add_rank_info_database(rank, title, score, visit, up, up_id, av_id, url)
+        add_rank_info_database(rank, title, score, visit, up, up_id, av_id, uri)
+        # 添加对应弹幕信息到数据库
+        add_video_barrage(av_id, )
         # 打印信息
         print(f'{rank} {title} {av_id} {up} {up_id} \r\n Insert database ok ')
-    print(f'执行完毕共{len(items)}条数据...定时继续启动...')
+    # print(f'执行完毕共{len(items)}条数据...定时继续启动...')
 
 
-def add_rank_info_database(rank, title, score, visit, up, up_id, av_id, url):
+def add_rank_info_database(rank, title, score, visit, up, up_id, av_id, uri):
     con = sqlite3.connect('database/bilibili_rank.db3')
     cursor = con.cursor()
     sql = f'INSERT INTO rank_list(rank,title,score,visit,up,up_id,av_id,url,get_time)' \
-          f'VALUES(\'{rank}\',\'{title}\',\'{score}\',\'{visit}\',\'{up}\',\'{up_id}\',\'{av_id}\',\'{url}\',' \
+          f'VALUES(\'{rank}\',\'{title}\',\'{score}\',\'{visit}\',\'{up}\',\'{up_id}\',\'{av_id}\',\'{uri}\',' \
           f'datetime(\'now\')) '
     # 执行SQL语句
     cursor.execute(sql)
@@ -50,13 +52,30 @@ def add_rank_info_database(rank, title, score, visit, up, up_id, av_id, url):
     con.close()
 
 
+def add_video_barrage(av_id, barrage_list):
+    for barrage in barrage_list:
+        con = sqlite3.connect('database/bilibili_rank.db3')
+        cursor = con.cursor()
+        sql = f'INSERT INTO barrage_list(barrage_av,barrage,get_time) ' \
+              f'VALUES(\'{av_id}\',\'{barrage}\', datetime(\'now\'))'
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 提交事务
+        con.commit()
+        # 关闭光标对象
+        cursor.close()
+        # 关闭数据库连接
+        con.close()
+
+
 # 入口
 if __name__ == '__main__':
     job()
-    # 每天到指定的时间执行拉取
-    # timer = input('请输入每天定时获取的时间如\"20:00\":')
-    # schedule.every().day.at(timer).do(job)
-    # print('定时服务启动...')
-    # while True:
-    #     schedule.run_pending()  # 运行所有可运行的任务
-    #     time.sleep(1)
+
+# 每天到指定的时间执行拉取
+# timer = input('请输入每天定时获取的时间如\"20:00\":')
+# schedule.every().day.at(timer).do(job)
+# print('定时服务启动...')
+# while True:
+#     schedule.run_pending()  # 运行所有可运行的任务
+#     time.sleep(1)
