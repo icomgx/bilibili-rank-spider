7898c7b073142f01494831eb1f2dcc6fa4c6989f
fred913 <41846274+fredisapig@users.noreply.github.com>
2020-02-09 16:25:14 +0800

    fixed bug

--- main/Get-VideoBarrage.py
+++ main/Get-VideoBarrage.py
@@ -14,21 +14,21 @@ def download_page(url):
     res = requests.get(url=url, headers=headers)
     return res
 
-    # 根据视频的av号获取cid
-    def get_cid(av):
-        av = av.strip('av')
-        uri = f'https://api.bilibili.com/x/player/pagelist?aid={av}&jsonp=jsonp'
-        res = download_page(uri)
-        res_text = res.text
-        res_dict = json.loads(res_text)
-        cid = res_dict['data'][0]['cid']
-        return cid
+# 根据视频的av号获取cid
+def get_cid(av):
+    av = av.strip('av')
+    uri = f'https://api.bilibili.com/x/player/pagelist?aid={av}&jsonp=jsonp'
+    res = download_page(uri)
+    res_text = res.text
+    res_dict = json.loads(res_text)
+    cid = res_dict['data'][0]['cid']
+    return cid
 
-    # 根据cid请求弹幕
-    def get_barrage(self, cid):
-        url = f'https://api.bilibili.com/x/v1/dm/list.so?oid={cid}'
-        res = download_page(url)
-        res_xml = res.content.decode('utf-8')
-        pattern = re.compile('<d.*?>(.*?)</d>')
-        dan_mu_list = pattern.findall(res_xml)
-        return dan_mu_list
+# 根据cid请求弹幕
+def get_barrage(self, cid):
+    url = f'https://api.bilibili.com/x/v1/dm/list.so?oid={cid}'
+    res = download_page(url)
+    res_xml = res.content.decode('utf-8')
+    pattern = re.compile('<d.*?>(.*?)</d>')
+    dan_mu_list = pattern.findall(res_xml)
+    return dan_mu_list
