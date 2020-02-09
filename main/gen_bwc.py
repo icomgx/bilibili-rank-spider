a6ab7a9b89f89393c0345b465ca6b0c3a6433626
LewisGu <59410356+LewisGu@users.noreply.github.com>
2020-02-09 21:37:24 +0800

    Update by  LewisGu
    详见readme

--- main/gen_bwc.py
+++ main/gen_bwc.py
@@ -3,15 +3,13 @@ import jieba
 # 生成词云图的库
 from wordcloud import WordCloud
 
-
 # 分词
-def Participle(barrage_str):
+def word_separate(barrage_str):
     p_list = jieba.lcut(barrage_str)
     return p_list
 
-
-# 生存词云图
-def genWordCloud(p_list, filename):
+# 生成词云图
+def genWordCloud(p_list, word_cloud_filename):
     w_str = ' '.join(p_list)
     w_settings = {
         'font_path': 'msyh.ttc',  # 词云使用字体
@@ -23,16 +21,16 @@ def genWordCloud(p_list, filename):
     # 实例化云词对象
     wc = WordCloud(**w_settings).generate(w_str)
     # 保存到文件
-    wc.to_file(filename)
+    wc.to_file(word_cloud_filename)
 
 
 # 打开弹幕文件
-def read_file(filename):
+def read_barrage_file(filename):
     with open(filename, mode='r', encoding='utf-8') as f:
         barrages = f.read()
         return barrages
 
 
 # 开始主流程
-def start(barrage_filename, word_cloud_filename):
-    genWordCloud(Participle(read_file(barrage_filename)), word_cloud_filename)
+def bwc_main(barrage_filename, word_cloud_filename):
+    genWordCloud(word_separate(read_barrage_file(barrage_filename)), word_cloud_filename)
